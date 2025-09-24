941fc2f36fdc113bd38018a8acf1d5908a3b67e0
Bruh1dude <146774680+Bruh1dude@users.noreply.github.com>
2025-09-24 09:38:58 +0200

    Add files via upload

---
+++ Uloha 1.py
@@ -0,0 +1,8 @@
+
+a = int(input('Cislo a'))
+b = int(input('Cislo b'))
+
+if (a*b)> 100:
+    print("presiahol 100")
+else:
+    print("nepresiahol 100")
---
+++ Uloha 2.py
@@ -0,0 +1,7 @@
+
+x = int(input("Zadaj x"))
+if x==0:
+    print("neda sa")
+else:
+    a = 1 / x
+    print(a)
